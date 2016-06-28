914e23fca08af689896009518ab6391bfc90269e
Thomas Jongerius <thomas@jongeriusnet.nl>
2016-06-28 10:04:58 +0200

    Update sub

---
+++ ip.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python -tt
+"""
+Script to search for ip addresses.
+"""
+
+import logging
+
+__author__ = "Thomas Jongerius"
+__copyright__ = "Copyright 2016, Thomas Jongerius"
+__credits__ = ["Thomas Jongerius"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Thomas Jongerius"
+__email__ = "thomasjongerius@yaworks.nl"
+__status__ = "Development"
+
